0a723e6ae55bf8ff125fa8d433586aa0c401a1d2
rahmatillo9 <ismoilsayfiddinov06@gmail.com>
2025-03-09 14:40:19 +0300

    /

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import ThemeProvider from "@/components/theme-provider";
 import { SidebarProvider } from "@/context/SidebarContext";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/mobilNavigation";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
