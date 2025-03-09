4a1412625a8fd221e47280b3b7980bc3883764fb
rahmatillo9 <ismoilsayfiddinov06@gmail.com>
2025-03-09 14:19:03 +0300

    C:/Program Files/Git/

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,8 @@ import ThemeProvider from "@/components/theme-provider";
 import { SidebarProvider } from "@/context/SidebarContext";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/mobilNavigation";
-import Navbar from "@/components/navbar";
+import Navbar from '../components/navbar';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
